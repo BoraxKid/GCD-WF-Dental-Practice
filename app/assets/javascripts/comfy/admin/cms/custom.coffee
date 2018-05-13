a42998cbcf509b10a731f10e1119025a4434e3da
Victor Aubry <victor.aubry@epitech.eu>
2018-05-13 15:13:49 +0000

    Buttons

--- app/assets/javascripts/comfy/admin/cms/custom.coffee
+++ app/assets/javascripts/comfy/admin/cms/custom.coffee
@@ -14,12 +14,20 @@ $ ->
       {data: 'patient_id'}
       {data: 'duration'}
       {data: 'fee_paid'}
+      {data: 'actions', orderable: false}
+    ],
+    columnDefs: [
+      {
+        render: (d, t, r) ->
+          '<div class="btn-group btn-group-sm nav justify-content-center">' + d + '</div>'
+        targets: 5
+      }
     ]
 
     initComplete: ->
       @api().columns([1, 2]).every ->
         column = this
-        select = $('<select class="form-control form-control-sm"><option value=""></option></select>').appendTo($(column.footer()).empty()).on('change', ->
+        select = $('<select class="form-control form-control-sm"><option value="">All</option></select>').appendTo($(column.footer()).empty()).on('change', ->
           val = $.fn.dataTable.util.escapeRegex($(this).val())
           column.search((if val then '^' + val + '$' else ''), true, false).draw()
           return
--- app/datatables/appointment_datatable.rb
+++ app/datatables/appointment_datatable.rb
@@ -1,5 +1,7 @@
 class AppointmentDatatable < AjaxDatatablesRails::Base
 
+  def_delegators :@view, :link_to, :edit_admin_appointment_path, :admin_appointment_path
+
   def view_columns
     # Declare strings in this format: ModelName.column_name
     # or in aliased_join_table.column_name format
@@ -9,6 +11,7 @@ class AppointmentDatatable < AjaxDatatablesRails::Base
       patient_id: { source: "Patient.name" },
       duration: { source: "Appointment.duration" },
       fee_paid: { source: "Appointment.fee_paid" },
+      actions: { source: "actions", searchable: false, orderable: false },
       # id: { source: "User.id", cond: :eq },
       # name: { source: "User.name", cond: :like }
     }
@@ -22,6 +25,7 @@ class AppointmentDatatable < AjaxDatatablesRails::Base
         patient_id: record.patient.name,
         duration: record.duration,
         fee_paid: record.fee_paid,
+        actions: link_to('Edit', edit_admin_appointment_path(record), class: 'btn btn-outline-secondary') + link_to('Delete', admin_appointment_path(record), method: :delete, data: {confirm: 'Are you sure?'}, class: 'btn btn-danger'),
         DT_RowID: record.id,
         # example:
         # id: record.id,
